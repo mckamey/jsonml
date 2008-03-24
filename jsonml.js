7dec66c10597cc7535952985b4ce2608a7bc16a3
mckamey <stephen@mckamey.com>
2008-03-24 07:58:54 -0700

    - fixing JSLint complaints (redefined catch ex, safe for-in loops)

--- jsonml.js
+++ jsonml.js
@@ -55,114 +55,117 @@
 
 */
 
-/*element*/ Array.prototype.parseJsonML = function (/*element function(element)*/ filter) {
+if ("function" !== typeof Array.prototype.parseJsonML) {
+	/*element*/ Array.prototype.parseJsonML = function (/*element function(element)*/ filter) {
 
-	//attribute name mapping
-	var am = {
-		rowspan : "rowSpan",
-		colspan : "colSpan",
-		cellpadding : "cellPadding",
-		cellspacing : "cellSpacing",
-		tabindex : "tabIndex",
-		accesskey : "accessKey",
-		hidefocus : "hideFocus",
-		usemap : "useMap",
-		maxlength : "maxLength",
-		readonly : "readOnly",
-		contenteditable : "contentEditable"
-		// can add more attributes here as needed
-	};
+		//attribute name mapping
+		var am = {
+			rowspan : "rowSpan",
+			colspan : "colSpan",
+			cellpadding : "cellPadding",
+			cellspacing : "cellSpacing",
+			tabindex : "tabIndex",
+			accesskey : "accessKey",
+			hidefocus : "hideFocus",
+			usemap : "useMap",
+			maxlength : "maxLength",
+			readonly : "readOnly",
+			contenteditable : "contentEditable"
+			// can add more attributes here as needed
+		};
 
-	//addAttributes
-	/*void*/ function aa(/*element*/ el, /*Object*/ a) {
-		// for each attributeName
-		for (var an in a) {
-			if (!an || typeof(a[an]) !== "string") {
-				continue;
-			}
-			if (an.toLowerCase() === "style") {
-				if ("undefined" !== typeof el.style.cssText) {
-					el.style.cssText = a[an];
-				} else {
-					el.style = a[an];
-				}
-			} else if (an.toLowerCase() === "class") {
-				el.className = a[an];
-			} else if (am[an.toLowerCase()]) {
-				el.setAttribute(am[an.toLowerCase()], a[an]);
-			} else {
-				el.setAttribute(an, a[an]);
-			}
-		}
-	}
-
-	//appendChild
-	/*void*/ function ac(/*element*/ el, /*Array or String*/ c) {
-		var ct, tb;
-		if (c) {
-			if (el.tagName.toLowerCase() === "table" && el.tBodies) {
-				// in IE must explicitly nest TDs in TBODY
-				ct = c.tagName ? c.tagName.toLowerCase() : null;// child tagName
-				if (ct && ct!=="tbody" && ct!=="thead") {
-					// insert in last tbody
-					tb = el.tBodies.length>0 ? el.tBodies[el.tBodies.length-1] : null;// tBody
-					if (!tb) {
-						tb = document.createElement("tbody");
-						el.appendChild(tb);
+		//addAttributes
+		/*void*/ function aa(/*element*/ el, /*Object*/ a) {
+			// for each attributeName
+			for (var an in a) {
+				if (an && typeof(a[an]) === "string") {
+					if (an.toLowerCase() === "style") {
+						if ("undefined" !== typeof el.style.cssText) {
+							el.style.cssText = a[an];
+						} else {
+							el.style = a[an];
+						}
+					} else if (an.toLowerCase() === "class") {
+						el.className = a[an];
+					} else if (am[an.toLowerCase()]) {
+						el.setAttribute(am[an.toLowerCase()], a[an]);
+					} else {
+						el.setAttribute(an, a[an]);
 					}
-					tb.appendChild(c);
 				}
-			} else {
-				el.appendChild(c);
 			}
 		}
-	}
 
-	//parseJsonML
-	/*element*/ function p(/*JsonML*/ jml) {
-		if (!jml) {
+		//appendChild
+		/*void*/ function ac(/*element*/ el, /*Array or String*/ c) {
+			var ct, tb;
+			if (c) {
+				if (el.tagName.toLowerCase() === "table" && el.tBodies) {
+					// in IE must explicitly nest TDs in TBODY
+					ct = c.tagName ? c.tagName.toLowerCase() : null;// child tagName
+					if (ct && ct!=="tbody" && ct!=="thead") {
+						// insert in last tbody
+						tb = el.tBodies.length>0 ? el.tBodies[el.tBodies.length-1] : null;// tBody
+						if (!tb) {
+							tb = document.createElement("tbody");
+							el.appendChild(tb);
+						}
+						tb.appendChild(c);
+					}
+				} else {
+					el.appendChild(c);
+				}
+			}
+		}
+
+		//parseJsonML
+		/*element*/ function p(/*JsonML*/ jml) {
+			if (!jml) {
+				return null;
+			}
+			if (typeof(jml) === "string") {
+				return document.createTextNode(jml);
+			}
+
+			if (!(jml instanceof Array) || jml.length < 1 || typeof(jml[0]) !== "string") {
+				throw new Error("parseJsonML");
+			}
+
+			var t = jml[0]; // tagName
+			var x = (t.toLowerCase() === "script"); // check for scripts
+			var el = x ? null : document.createElement(t);
+
+			for (var i=1; i<jml.length; i++) {
+				if (!x) {
+					if (jml[i] instanceof Array || typeof(jml[i]) === "string") {
+						// append children
+						ac(el, p(jml[i]));
+					} else if (typeof(jml[i]) === "object") {
+						// add attributes
+						aa(el, jml[i]);
+					}
+				//} else if (typeof(jml[i]) === "string") {
+					/*	JSLint: "eval is evil"
+						uncomment at your own risk, executes script elements */
+					//eval( "(" + jml[i] + ")" );
+				}
+			}
+
+			return (el && "function" === typeof filter) ? filter(el) : el;
+		}
+
+		return p(this);
+	};
+}
+
+if ("function" !== typeof String.prototype.parseJsonML) {
+	/*element*/ String.prototype.parseJsonML = function (/*function(element)*/ filter) {
+		var jml;
+		try {
+			jml = this.parseJSON();
+		} catch (ex) {
 			return null;
 		}
-		if (typeof(jml) === "string") {
-			return document.createTextNode(jml);
-		}
-
-		if (!(jml instanceof Array) || jml.length < 1 || typeof(jml[0]) !== "string") {
-			throw new Error("parseJsonML");
-		}
-
-		var t = jml[0]; // tagName
-		var x = (t.toLowerCase() === "script"); // check for scripts
-		var el = x ? null : document.createElement(t);
-
-		for (var i=1; i<jml.length; i++) {
-			if (!x) {
-				if (jml[i] instanceof Array || typeof(jml[i]) === "string") {
-					// append children
-					ac(el, p(jml[i]));
-				} else if (typeof(jml[i]) === "object") {
-					// add attributes
-					aa(el, jml[i]);
-				}
-			//} else if (typeof(jml[i]) === "string") {
-				/*	JSLint: "eval is evil"
-					uncomment at your own risk, executes script elements */
-				//eval( "(" + jml[i] + ")" );
-			}
-		}
-
-		return (el && "function" === typeof filter) ? filter(el) : el;
-	}
-
-	return p(this);
-};
-
-/*element*/ String.prototype.parseJsonML = function (/*function(element)*/ filter) {
-	var jml;
-	try {
-		jml = this.parseJSON();
-	} catch (ex) {
-		return null;
-	}
-	return (jml instanceof Array) ? jml.parseJsonML(filter) : null;
-};
+		return (jml instanceof Array) ? jml.parseJsonML(filter) : null;
+	};
+}
