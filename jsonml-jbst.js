0cc9f1599b57ea3e93ee48ce1a305f87268d084e
mckamey <stephen@mckamey.com>
2008-08-02 15:03:48 -0700

    - simplifying JBST API - improving compaction / obfuscation

--- jsonml-jbst.js
+++ jsonml-jbst.js
@@ -3,7 +3,7 @@
 	JsonML Browser Side Templates
 	Copyright (c)2006-2008 Stephen M. McKamey
 	Created: 2008-07-28-2337
-	Modified: 2008-08-01-2338
+	Modified: 2008-08-02-1501
 \*---------------------------------------------------------*/
 
 /* namespace JBST */
@@ -11,68 +11,75 @@ if ("undefined" === typeof JBST) {
 	window.JBST = {};
 }
 
-/*JsonML*/ JBST.dataBind = function(/*JBST*/ template, /*object*/ data, /*int*/ index) {
+// combines JBST and JSON to produce JsonML
+/*JsonML*/ JBST.dataBind = function(/*JBST*/ template, /*JSON*/ data, /*int*/ index) {
 	// NOTE: it is very important to add transformations to a copy of the template
 	// nodes, otherwise it destroys the original template.
 
-	// recursively apply to all nodes of the template graph
-	/*object*/ function dataBind(/*object*/ node, /*object*/ data, /*int*/ index) {
-		if (node) {
-			if ("function" === typeof node) {
-				// this corresponds to the single $
-				return node(
+	// recursively applies dataBind to all nodes of the template graph
+	/*object*/ function db(/*JBST*/ t, /*JSON*/ d, /*int*/ n) {
+		// JBST node
+		if (t) {
+			if ("function" === typeof t) {
+				// this corresponds to the $item parameter
+				return t(
 					{
-						data: data,
-						index: isFinite(index) ? Number(index) : -1
+						data: d,
+						index: isFinite(n) ? Number(n) : -1
 					});
 			}
 
-			var output;
-			if (node instanceof Array) {
-				output = [];
-				for (var i=0; i<node.length; i++) {
-					var result = dataBind(node[i], data, index);
-					if (result instanceof Array && result.$JBST) {
-						output = output.concat(result);
-					} else if ("object" === typeof result) {
-						output.push(result);
+			var o;
+			if (t instanceof Array) {
+				// output array
+				o = [];
+				for (var i=0; i<t.length; i++) {
+					// result
+					var r = db(t[i], d, n);
+					if (r instanceof Array && r.$JBST) {
+						// result was multiple JsonML trees
+						o = o.concat(r);
+					} else if ("object" === typeof r) {
+						// result was a JsonML tree
+						o.push(r);
 					} else {
-						output.push(String(result));
+						// must convert to string or JsonML will discard
+						o.push(String(r));
 					}
 				}
-				return output;
+				return o;
 			}
 
-			if ("object" === typeof node) {
-				output = {};
-				for (var key in node) {
-					if (node.hasOwnProperty(key)) {
-						output[key] = dataBind(node[key], data, index);
+			if ("object" === typeof t) {
+				// output object
+				o = {};
+				// for each property in node
+				for (var p in t) {
+					if (t.hasOwnProperty(p)) {
+						o[p] = db(t[p], d, n);
 					}
 				}
-				return output;
+				return o;
 			}
 		}
 
-		// rest are value types, so return directly
-		return node;
+		// rest are value types, so return node directly
+		return t;
 	}
-
-	return dataBind(template, data, index);
-};
-
-/*JsonML*/ JBST.dataBindEach = function(/*JsonML*/ template, /*array*/ data) {
-	var output = [];
-
-	// TODO: need some sort of flag here, instanceof?
-	output.$JBST = true;
 
 	if (data instanceof Array) {
+		var o = [];
+
+		// flag container to differentiate from JsonML
+		o.$JBST = true;
+
 		for (var i=0; i<data.length; i++) {
-
-			output[i] = JBST.dataBind(template, data[i], i);
+			// apply template to each item in array
+			o[i] = db(template, data[i], i);
 		}
+		return o;
+	} else {
+		// data is singular to apply template once
+		return db(template, data, index);
 	}
-
-	return output;
 };
