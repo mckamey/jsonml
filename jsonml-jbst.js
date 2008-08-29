bc2bb099674a51e0fe3fb8c2e4390f8aa15ada28
mckamey <stephen@mckamey.com>
2008-08-29 10:22:40 -0700

    - overhauled JsonML+BST to allow instance members on templates

--- jsonml-jbst.js
+++ jsonml-jbst.js
@@ -4,14 +4,14 @@
 	JsonML Browser-Side Templating
 
 	Created: 2008-07-28-2337
-	Modified: 2008-08-09-0930
+	Modified: 2008-08-29-0056
 
 	Released under an open-source license:
 	http://jsonml.org/License.htm
 
-    This file creates a global JsonML object containing this method:
+    This file creates a JsonML.BST type containing this method:
 
-        JsonML.dataBind(template, data)
+	new JsonML.BST(template).dataBind(data)
 */
 
 /* namespace JsonML */
@@ -19,28 +19,35 @@ if ("undefined" === typeof JsonML) {
 	window.JsonML = {};
 }
 
-// combines JsonML+BST and JSON to produce JsonML
-/*JsonML*/ JsonML.dataBind = function(/*JsonML+BST*/ jbst, /*JSON*/ data) {
+JsonML.BST = function(/*JsonML+BST*/ jbst) {
+	var self = this;
 
-	// recursively apply dataBind to all nodes of the template graph
+	// unique counter for generated method names
+	var g = 0;
+
+	// recursively applies dataBind to all nodes of the template graph
 	// NOTE: it is very important to replace each node with a copy,
 	// otherwise it destroys the original template.
-
-	/*object*/ function db(/*JsonML+BST*/ t, /*JSON*/ d, /*int*/ n) {
-		// don't bind to empty data
-		if ("undefined" === typeof d || d === null) {
-			return null;
-		}
-
-		// for each JsonML+BST node
+	/*object*/ function db(/*JsonML+BST*/ t, /*object*/ d, /*int*/ n) {
+		// process JsonML+BST node
 		if (t) {
 			if ("function" === typeof t) {
-				// this corresponds to the $item parameter
-				return t(
-					{
-						data: d,
-						index: isFinite(n) ? Number(n) : -1
-					});
+				// temporary method name using a counter to
+				// avoid collisions when recursively calling
+				var m = "$jbst_"+(g++)+"";
+				try {
+					// setup context for code block
+					self[m] = t;
+					self.data = d;
+					self.index = isFinite(n) ? Number(n) : -1;
+					// execute in the context of template as "this"
+					return self[m]();
+				} finally {
+					g--;
+					delete self[m];
+					delete self.data;
+					delete self.index;
+				}
 			}
 
 			var o;
@@ -57,7 +64,7 @@ if ("undefined" === typeof JsonML) {
 					} else if ("object" === typeof r) {
 						// result was a JsonML tree
 						o.push(r);
-					} else {
+					} else if ("undefined" !== typeof r && r !== null) {
 						// must convert to string or JsonML will discard
 						o.push(String(r));
 					}
@@ -82,17 +89,21 @@ if ("undefined" === typeof JsonML) {
 		return t;
 	}
 
-	if (data instanceof Array) {
-		// create a document fragment to hold list
-		var o = [""];
+	// the publicly exposed instance method
+	// combines JsonML+BST and JSON to produce JsonML
+	/*JsonML*/ this.dataBind = function(/*object*/ data, /*int*/ index) {
+		if (data instanceof Array) {
+			// create a document fragment to hold list
+			var o = [""];
 
-		for (var i=0; i<data.length; i++) {
-			// apply template to each item in array
-			o.push(db(jbst, data[i], i));
+			for (var i=0; i<data.length; i++) {
+				// apply template to each item in array
+				o.push(db(jbst, data[i], i));
+			}
+			return o;
+		} else {
+			// data is singular to apply template once
+			return db(jbst, data, index);
 		}
-		return o;
-	} else {
-		// data is singular to apply template once
-		return db(jbst, data, -1);
-	}
+	};
 };
